4b7908d3c32f62d156a919847f55a03163bf97ef
Mason Stone <maso27@users.noreply.github.com>
2021-10-07 14:52:40 -0600

    Swapping direction of Volume knobs
    "clockwise" is backwards on right board.  Had to reverse directions for volume up and down.

--- keyboards/sofle/keymaps/maso27/encoder.c
+++ keyboards/sofle/keymaps/maso27/encoder.c
@@ -37,13 +37,13 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else if (index == 1) {
         if (clockwise) {
             if(shift_held) {
-                tap_code(KC_VOLU);
+                tap_code(KC_VOLD);
             } else {
                 tap_code(KC_DOWN);
             }
         } else {
             if(shift_held) {
-                tap_code(KC_VOLD);
+                tap_code(KC_VOLU);
             } else {
                 tap_code(KC_UP);
             }
