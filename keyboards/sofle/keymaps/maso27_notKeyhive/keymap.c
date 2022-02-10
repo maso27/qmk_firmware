813137f6ee61900c51b4223a988033cc03a462cc
maso27 <madmaso+git@gmail.com>
2022-02-10 15:08:01 -0700

    Replacing Esc with Grave-Esc key

--- keyboards/sofle/keymaps/maso27_notKeyhive/keymap.c
+++ keyboards/sofle/keymaps/maso27_notKeyhive/keymap.c
@@ -37,8 +37,8 @@ enum sofle_layers {
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
     KC_GAMING,
-    KC_LOWER,
-    KC_RAISE,
+    KC_LOWER = MO(_LOWER), // LT(_LOWER,KC_LPRN), // lower layer when held, parentheses when tapped
+    KC_RAISE = MO(_RAISE), // LT(_RAISE,KC_RPRN),
     KC_PRVWD = LCTL(KC_LEFT),
     KC_NXTWD = LCTL(KC_RIGHT),
     KC_DWORD = LCTL(KC_BSPC),
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * MASO QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |ESC/~ |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -60,16 +60,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LCTR |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Ent/Shft |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | RCTR |LOWER | / Bspc  /       \Space \  |Raise |  `   |  =   |  \   |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            |      |      |      |  (   |/       /         \      \ |  )   |      |      |      |
  *            `---------------------------'-------'           '------''---------------------------'
  */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, // KC_BSPC_DEL,
+  KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_TAB,    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, // KC_BSPC_DEL,
   KC_LSFT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     TG(_GAMING),KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_SFTENT,
-                 KC_LGUI,KC_LALT, KC_RCTRL, MO(_LOWER), KC_BSPC,   KC_SPC, MO(_RAISE), KC_GRV, KC_EQL, KC_BSLS
+  KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE, TG(_GAMING),KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_SFTENT,
+                 KC_LGUI,KC_LALT, KC_RCTRL, KC_LOWER, KC_BSPC,   KC_SPC, KC_RAISE, KC_GRV, KC_EQL, KC_BSLS
 ),
 
 
