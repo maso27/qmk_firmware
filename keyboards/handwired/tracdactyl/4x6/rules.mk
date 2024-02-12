c4d308989966cf908d83b51e9d4fe75484a286ef
maso27 <madmaso+git@gmail.com>
2024-02-12 12:33:14 -0700

    try toggle and caps_word

--- keyboards/handwired/tracdactyl/4x6/rules.mk
+++ keyboards/handwired/tracdactyl/4x6/rules.mk
@@ -7,6 +7,7 @@ RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default
 
 SPLIT_KEYBOARD = yes
+SPLIT_LAYER_STATE_ENABLE = yes # transmit layer info to other half for RGB
 
 POINTING_DEVICE_DRIVER = pmw3389
 
