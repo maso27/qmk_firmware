dd198882a0f5de406ae091d6860f14e71c934932
maso27 <madmaso+git@gmail.com>
2024-04-16 09:55:48 -0600

    switched master to right side

--- keyboards/handwired/tracdactyl/config.h
+++ keyboards/handwired/tracdactyl/config.h
@@ -37,7 +37,7 @@
 #define POINTING_DEVICE_INVERT_X
 
 /* Handedness. */
-#define MASTER_LEFT
+#define MASTER_RIGHT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_LEFT above.
